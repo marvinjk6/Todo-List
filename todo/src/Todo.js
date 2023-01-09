b44205f673c6d64d1797ca4b5bb7b688a0361b90
marvinjk6 <marvin.tavares10@gmail.com>
2023-01-09 19:03:04 -0300

    Adicionando itens na lista

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -3,11 +3,22 @@ import './Todo.css';
 
 function Todo() {
 
-    // o estado do text começa vazio
+    // o estado inicial de items é um array vazio
+    const [items, setItems] = useState([]);
     const [text, setText] = useState('');
 
-    // pegando o valor do input
-    // Todo evento possui target e value 
+    function addItem(e) {
+        // o comportamento padrão do botão de um formulário é assim que for clicado enviar os dados e atualiza a página, vamos impedir esse comportamento.
+        e.preventDefault();
+
+        // o if(text) quer dizer, se o input tiver um valor 'se ele for preenchido', aí sim vai adicionar ao array items, caso o contrário não
+        if(text) {
+             // o array vai receber o que já contem em items + valor de text, que é o valor do input
+        setItems([...items, text]);
+        }
+       
+    }
+
 
     function handleChange(e) {
         let inputValue = e.target.value;
@@ -18,12 +29,12 @@ function Todo() {
         <div className='container'>
             <h1>Todo-List</h1>
             <form>
-                <input onChange={handleChange}></input>
-                <button>add</button>
+                <input value={text} onChange={handleChange}></input>
+                <button onClick={addItem}>add</button>
             </form>
 
-            <ul> {/*  passando text como item da lista */}
-                <li>{text}</li>
+            <ul> {/*  items, que é um array e tem a função map, que cria um array */}
+                {items.map(item => <li>{item}</li>)}
             </ul>
         </div>
     )
