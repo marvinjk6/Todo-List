1a2ab988cbcbbbd1551003ce1915a5bb5c42aa6c
marvinjk6 <marvin.tavares10@gmail.com>
2023-01-09 19:13:32 -0300

    Separando o component List do Todo

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -1,24 +1,20 @@
 import React, { useState } from 'react';
+import List from './components/List'; // importar list
 import './Todo.css';
 
 function Todo() {
 
-    // o estado inicial de items é um array vazio
     const [items, setItems] = useState([]);
     const [text, setText] = useState('');
 
     function addItem(e) {
-        // o comportamento padrão do botão de um formulário é assim que for clicado enviar os dados e atualiza a página, vamos impedir esse comportamento.
         e.preventDefault();
-
-        // o if(text) quer dizer, se o input tiver um valor 'se ele for preenchido', aí sim vai adicionar ao array items, caso o contrário não
         if(text) {
-             // o array vai receber o que já contem em items + valor de text, que é o valor do input
-        setItems([...items, text]);
+            setItems([...items, text]);
+            setText(''); // vai receber vazio depois de adicionar o text em items para limpar o input
         }
        
     }
-
 
     function handleChange(e) {
         let inputValue = e.target.value;
@@ -33,11 +29,11 @@ function Todo() {
                 <button onClick={addItem}>add</button>
             </form>
 
-            <ul> {/*  items, que é um array e tem a função map, que cria um array */}
-                {items.map(item => <li>{item}</li>)}
-            </ul>
+            {/* List precisa ter acesso a items, foi criado a propriedade items que recebe o array items */}
+            <List items={items}></List>
         </div>
     )
 }
+
 
 export default Todo;
---
+++ todo/src/components/List.js
@@ -0,0 +1,13 @@
+
+
+function List(props) {
+
+    return (
+        <ul>
+            {props.items.map(item => <li>{item}</li>)}
+        </ul>
+    )
+
+}
+
+export default List;
