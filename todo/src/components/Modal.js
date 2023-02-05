0e1fc9fa5565cabbd7da980b2b9b49788bcc4099
marvinjk6 <marvin.tavares10@gmail.com>
2023-02-05 20:45:36 -0300

    Ajuste para quando clicar fora do modal o mesmo sair da tela

--- todo/src/components/Modal.js
+++ todo/src/components/Modal.js
@@ -6,12 +6,12 @@ function Modal(props) {
     function hideModal(e) {
         let target = e.target;
         if(target.id === 'modal') {
-            props.onHideModal();
+            props.onHideModal()
         }
     }
 
     return(
-        <div id='modal' onClick={()=>hideModal(true)} className={props.showModal?'modal':'modal hideModal'}>
+        <div id='modal' onClick={hideModal} className={props.showModal?'modal':' hideModal'}>
             <Card className='cardModal'>
                 {props.children}
             </Card>
